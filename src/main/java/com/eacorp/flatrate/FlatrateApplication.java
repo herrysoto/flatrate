a74587214fce2c2e12fce441368dfe2aaacbe048
hsoto <hsoto>
2017-05-03 17:05:45 -0500

    configurando el pom

--- src/main/java/com/eacorp/flatrate/FlatrateApplication.java
+++ src/main/java/com/eacorp/flatrate/FlatrateApplication.java
@@ -9,17 +9,20 @@ import org.apache.tomcat.util.descriptor.web.ContextResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.jndi.JndiObjectFactoryBean;
 
 @SpringBootApplication
-public class FlatrateApplication {
+public class FlatrateApplication extends SpringBootServletInitializer {
 
 	@Value("${spring.datasource.driver-class-name}")
 	private String driverClassName;
@@ -30,64 +33,69 @@ public class FlatrateApplication {
 	@Value("${spring.datasource.url}")
 	private String url;
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	       return application.sources(FlatrateApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(FlatrateApplication.class, args);
 				
 	}
-	
-	@Bean
-	public TomcatEmbeddedServletContainerFactory tomcatFactory() {
-	    return new TomcatEmbeddedServletContainerFactory() {
-
-	        @Override
-	        protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
-	                Tomcat tomcat) {
-	            tomcat.enableNaming();
-	            return super.getTomcatEmbeddedServletContainer(tomcat);
-	        }
-	    };
-	}
-	
-	
-
-@Bean
-public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
-    return new EmbeddedServletContainerCustomizer() {
-        @Override
-        public void customize(ConfigurableEmbeddedServletContainer container) {
-            if (container instanceof TomcatEmbeddedServletContainerFactory) {
-                TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory = (TomcatEmbeddedServletContainerFactory) container;
-                tomcatEmbeddedServletContainerFactory.addContextCustomizers(new TomcatContextCustomizer() {
-                    @Override
-                    public void customize(Context context) {
-                    	ContextResource mydatasource = new ContextResource();
-                        mydatasource.setName("jdbc/prueba");
-                        mydatasource.setAuth("Container");
-                        mydatasource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
-                        mydatasource.setType("javax.sql.DataSource");
-                        mydatasource.setScope("Sharable");
-                        mydatasource.setProperty("driverClassName", driverClassName);
-                        mydatasource.setProperty("url", url);
-                        mydatasource.setProperty("username", user);
-                        mydatasource.setProperty("password", password);
-
-                        context.getNamingResources().addResource(mydatasource);
-
-                    }
-                });
-            }
-        }
-    };
-}
+//	
+//	@Bean
+//	public TomcatEmbeddedServletContainerFactory tomcatFactory() {
+//	    return new TomcatEmbeddedServletContainerFactory() {
+//
+//	        @Override
+//	        protected TomcatEmbeddedServletContainer getTomcatEmbeddedServletContainer(
+//	                Tomcat tomcat) {
+//	            tomcat.enableNaming();
+//	            return super.getTomcatEmbeddedServletContainer(tomcat);
+//	        }
+//	    };
+//	}
+//	
+//	
+//
+//@Bean
+//public EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
+//    return new EmbeddedServletContainerCustomizer() {
+//        @Override
+//        public void customize(ConfigurableEmbeddedServletContainer container) {
+//            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+//                TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory = (TomcatEmbeddedServletContainerFactory) container;
+//                tomcatEmbeddedServletContainerFactory.addContextCustomizers(new TomcatContextCustomizer() {
+//                    @Override
+//                    public void customize(Context context) {
+//                    	ContextResource mydatasource = new ContextResource();
+//                        mydatasource.setName("jdbc/prueba");
+//                        mydatasource.setAuth("Container");
+//                        mydatasource.setProperty("factory", "org.apache.tomcat.jdbc.pool.DataSourceFactory");
+//                        mydatasource.setType("javax.sql.DataSource");
+//                        mydatasource.setScope("Sharable");
+//                        mydatasource.setProperty("driverClassName", driverClassName);
+//                        mydatasource.setProperty("url", url);
+//                        mydatasource.setProperty("username", user);
+//                        mydatasource.setProperty("password", password);
+//
+//                        context.getNamingResources().addResource(mydatasource);
+//
+//                    }
+//                });
+//            }
+//        }
+//    };
+//}
 
 @Lazy
 @Bean(destroyMethod="")
 public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
-    JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
-    bean.setJndiName("java:comp/env/jdbc/prueba");
-    bean.setProxyInterface(DataSource.class);
-    bean.setLookupOnStartup(false);
-    bean.afterPropertiesSet();
-    return (DataSource)bean.getObject();
+	DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    dataSourceBuilder.url(url);
+    dataSourceBuilder.username(user);
+    dataSourceBuilder.password(password);
+    dataSourceBuilder.driverClassName(driverClassName);
+    return dataSourceBuilder.build();  
 }
 }
